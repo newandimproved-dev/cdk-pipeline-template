f75d51ac20a5cbcf66a9578c01d14161f5d9e175
Mark Kang <markkang@Marks-MacBook-Pro.local>
2022-06-05 16:23:56 -0700

    Initialize CDK package

---
+++ test/cdk-pipeline-template.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkPipelineTemplate from '../lib/cdk-pipeline-template-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-pipeline-template-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkPipelineTemplate.CdkPipelineTemplateStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
