57d7f4ca0572fa5053f0b7043c70a0e99fec23c5
Mark Kang <markkang@Marks-MacBook-Pro.local>
2022-06-05 19:19:56 -0700

    Set crossAccountKeys to true

--- lib/cdk-pipeline-template-stack.ts
+++ lib/cdk-pipeline-template-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { PipelineAppStage } from './stage';
 
 export class CdkPipelineTemplateStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -8,6 +9,7 @@ export class CdkPipelineTemplateStack extends Stack {
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
+      crossAccountKeys: true,
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.connection('newandimproved-dev/cdk-pipeline-template', 'main', {
           connectionArn: 'arn:aws:codestar-connections:us-west-2:714496019310:connection/769cf32f-d119-4779-859b-77a3f0650947'
@@ -15,5 +17,9 @@ export class CdkPipelineTemplateStack extends Stack {
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+
+    pipeline.addStage(new PipelineAppStage(this, "beta", {
+      env: { account: "550575751237", region: "us-west-2" }
+    }));
   }
 }
