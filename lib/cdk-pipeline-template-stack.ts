7501b8ad255159b2ef4c45462688776e891dcb2a
Mark Kang <newandimproved.dev@gmail.com>
2022-06-06 16:46:21 -0700

    Remove output directory for lambda build

--- lib/cdk-pipeline-template-stack.ts
+++ lib/cdk-pipeline-template-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep, CodeBuildStep} from 'aws-cdk-lib/pipelines';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild'
+import * as iam from 'aws-cdk-lib/aws-iam'
 import { PipelineAppStage } from './stage';
 
 export class CdkPipelineTemplateStack extends Stack {
@@ -26,7 +27,7 @@ export class CdkPipelineTemplateStack extends Stack {
           connectionArn: 'arn:aws:codestar-connections:us-west-2:714496019310:connection/769cf32f-d119-4779-859b-77a3f0650947'
         }),
         additionalInputs: {
-          './lib/lambda': lambdaBuildStep.addOutputDirectory('./lib/lambda')
+          './lib/lambda': lambdaBuildStep
         },
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
