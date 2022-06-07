0ef406d576da3acd78c0e1c8e5df542fc71dc727
Mark Kang <newandimproved.dev@gmail.com>
2022-06-06 20:07:16 -0700

    Fix primary output directory

--- lib/cdk-pipeline-template-stack.ts
+++ lib/cdk-pipeline-template-stack.ts
@@ -9,13 +9,15 @@ export class CdkPipelineTemplateStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const lambdaCodePipelineSource = CodePipelineSource.connection('newandimproved-dev/cdk-lambda-template', 'main', {
+      connectionArn: 'arn:aws:codestar-connections:us-west-2:714496019310:connection/769cf32f-d119-4779-859b-77a3f0650947'
+    });
+
     const lambdaBuildStep = new CodeBuildStep('BuildLambda', {
       buildEnvironment: {
           buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_3
       },
-      input: CodePipelineSource.connection('newandimproved-dev/cdk-lambda-template', 'main', {
-        connectionArn: 'arn:aws:codestar-connections:us-west-2:714496019310:connection/769cf32f-d119-4779-859b-77a3f0650947'
-      }),
+      input: lambdaCodePipelineSource,
       commands: ['mvn clean install', 'mvn package']
   })
 
@@ -27,7 +29,7 @@ export class CdkPipelineTemplateStack extends Stack {
           connectionArn: 'arn:aws:codestar-connections:us-west-2:714496019310:connection/769cf32f-d119-4779-859b-77a3f0650947'
         }),
         additionalInputs: {
-          'lambda': lambdaBuildStep.addOutputDirectory('lambda')
+          '/lib/lambda': lambdaBuildStep.primaryOutputDirectory(''),
         },
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
