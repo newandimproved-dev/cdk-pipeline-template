f4a7d3a1b538d93a9f535d253eef6544d5a8f96c
Mark Kang <newandimproved.dev@gmail.com>
2022-06-06 18:05:30 -0700

    Using parent directory for lambda

--- lib/application-stack.ts
+++ lib/application-stack.ts
@@ -9,7 +9,7 @@ export class ApplicationStack extends cdk.Stack {
       new Function(this, 'LambdaFunction', {
         runtime: Runtime.JAVA_11,
         handler: 'CdkLambdaTemplate.Handler',
-        code: Code.fromAsset("lambda")
+        code: Code.fromAsset("../lib/lambda")
       });
     }
 }
