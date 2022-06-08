2ee287f9fbbfe9b7f4c1cc707a8503262e1f769a
Mark Kang <newandimproved.dev@gmail.com>
2022-06-07 18:08:47 -0700

    Try subfolder

--- lib/cdk-pipeline-template-stack.ts
+++ lib/cdk-pipeline-template-stack.ts
@@ -31,7 +31,7 @@ export class CdkPipelineTemplateStack extends Stack {
       synth: new ShellStep('Synth', {
         input: cdkCodePipelineSource,
         additionalInputs: {
-          'lambda': lambdaBuildStep.addOutputDirectory(''),
+          'lambda': lambdaBuildStep.primaryOutputDirectory('lambda'),
         },
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
